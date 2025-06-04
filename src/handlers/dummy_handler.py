752bd3faebfe3194200f9ad2fd6e6b44b75fda0c
Chooook <egr311@ya.ru>
2025-06-04 21:48:25 +0400

    test: add dummy tasks handler

---
+++ src/handlers/dummy_handler.py
@@ -0,0 +1,8 @@
+from random import shuffle
+
+
+async def _dummy_handler(task: dict) -> str:
+    """Handle task dummy"""
+    prompt = list(task['prompt'])
+    shuffle(prompt)
+    return str(prompt)
