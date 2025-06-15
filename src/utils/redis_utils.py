74b8490afb0424b31f60be7c1d7e476584c606dd
Chooook <egr311@ya.ru>
2025-06-15 14:18:06 +0400

    fix: add recovered tasks to the queue end

--- src/utils/redis_utils.py
+++ src/utils/redis_utils.py
@@ -9,13 +9,14 @@ from schemas.task import Task
 
 
 async def recover_tasks(redis: Redis):
-    logger.info("🔍 Восстановление незавершенных задач...")
+    # FIXME: can make problems if func called when worker handling this tasks
+    logger.info('🔍 Восстановление незавершенных задач...')
     while True:
         task_id = await redis.rpop('processing_queue')
         if not task_id:
             break
-        await redis.rpush('task_queue', task_id)
-        logger.info(f"♻️ Восстановлена задача: {task_id}")
+        await redis.lpush('task_queue', task_id)
+        logger.info(f'♻️ Восстановлена задача: {task_id}')
 
 
 async def mark_task_failed(redis: Redis, task_id: str, error_msg: str):
@@ -31,7 +32,7 @@ async def mark_task_failed(redis: Redis, task_id: str, error_msg: str):
 async def cleanup_dlq(redis: Redis):
     while True:
         await asyncio.sleep(3600)
-        logger.info("🧹 Очистка dead_letters...")
+        logger.info('🧹 Очистка dead_letters...')
         dlq_length = await redis.llen('dead_letters')
         if dlq_length > 50:
             tasks = await redis.lrange('dead_letters', 0, -1)
