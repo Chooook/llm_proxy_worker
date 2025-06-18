a55fc330053f82cadb03d81bd30352b18ddd170b
Chooook <egr311@ya.ru>
2025-06-18 23:00:15 +0400

    chore: remove unused mark failed func

--- src/utils/redis_utils.py
+++ src/utils/redis_utils.py
@@ -1,11 +1,7 @@
 import asyncio
-from datetime import datetime, timezone
 
 from redis.asyncio import Redis
 from loguru import logger
-
-from schemas.answer import Answer
-from schemas.task import Task
 
 
 async def recover_tasks(redis: Redis):
@@ -17,16 +13,6 @@ async def recover_tasks(redis: Redis):
             break
         await redis.lpush('task_queue', task_id)
         logger.info(f'♻️ Восстановлена задача: {task_id}')
-
-
-async def mark_task_failed(redis: Redis, task_id: str, error_msg: str):
-    task_data = await redis.get(f'task:{task_id}')
-    if task_data:
-        task = Task.model_validate_json(task_data)
-        task.status = 'failed'
-        task.finished_at = datetime.now(timezone.utc).isoformat()
-        task.error = Answer(text=error_msg)
-        await redis.setex(f'task:{task_id}', 86400, task.model_dump_json())
 
 
 async def cleanup_dlq(redis: Redis):
