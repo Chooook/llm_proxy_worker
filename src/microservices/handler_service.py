5ba8be5a1f69b8a9b8718ff46cee94604839d9ce
Chooook <egr311@ya.ru>
2025-07-18 17:08:06 +0400

    feat: add fastapi app template as string.Template, update generation func

--- src/microservices/handler_service.py
+++ src/microservices/handler_service.py
@@ -1,45 +1,49 @@
 import traceback
 from pathlib import Path
+from string import Template
 
 from loguru import logger
 
+FASTAPI_HANDLER_TEMPLATE = Template('''
+    import traceback
+    from fastapi import FastAPI, HTTPException
+
+    app = FastAPI()
+
+    @app.post('/process')
+    async def process_request(data: dict):
+        try:
+            from $module import $function
+            result = $function(data)
+            return {'result': result}
+        except Exception as e:
+            error_detail = {
+                'error': str(e),
+                'traceback': traceback.format_exc()
+            }
+            raise HTTPException(
+                status_code=500,
+                detail=error_detail
+            )
+
+    @app.get('/health')
+    async def health_check():
+        return {'status': 'ok'}
+''')
+
+
 def generate_fastapi_app(handler_dir: Path, handler_config):
-        """Генерирует файл FastAPI приложения с обработкой ошибок"""
-        try:  # TODO move to microservices utils
-            app_code = f'''
-import traceback
-from fastapi import FastAPI, HTTPException
-
-app = FastAPI()
-
-@app.post('/process')
-async def process_request(data: dict):
-    try:
-        from {handler_config.interface_func_module} import (
-            {handler_config.interface_func_name})
-        result = {handler_config.interface_func_name}(data)
-        return {{'result': result}}
-    except Exception as e:
-        error_detail = {{
-            'error': str(e),
-            'traceback': traceback.format_exc()
-        }}
-        raise HTTPException(
-            status_code=500,
-            detail=error_detail
-        )
-
-@app.get('/health')
-async def health_check():
-    return {{'status': 'ok'}}
-'''
+        """Генерирует файл FastAPI приложения"""
+        try:
+            app_code = FASTAPI_HANDLER_TEMPLATE.substitute(
+                module=handler_config.interface_func_module,
+                function=handler_config.interface_func_name)
             app_file = handler_dir / 'handler_app.py'
             app_file.write_text(app_code)
 
-        except Exception:
+        except Exception as e:
             logger.error(
                 f'‼️ Error generating FastAPI app '
-                f'for {handler_config.handler_id}:\n'
-                f'{traceback.format_exc()}'
-            )
+                f'for {handler_config.handler_id}: {e}')
+            logger.debug(f'{traceback.format_exc()}')
             raise
