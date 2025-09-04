f5d9ea1b2d1307701ef4f0b2949b7661c92566d1
Chooook <egr311@ya.ru>
2025-09-04 09:19:38 +0300

    feat: add git config check if handler dir exists to decide to clone or to update handler from repo

--- src/utils/git_utils.py
+++ src/utils/git_utils.py
@@ -1,5 +1,6 @@
 import asyncio
 import os
+import shutil
 from pathlib import Path
 
 from loguru import logger
@@ -10,10 +11,22 @@ GIT_PASS = os.getenv('GIT_PASS', '')
 
 
 async def ensure_repo(repo: str, branch: str, target_dir: Path):
-    if target_dir.exists():
+    if target_dir.exists() and check_repo_dir_config(target_dir, repo):
         await update_repo(repo, branch, target_dir)
     else:
+        shutil.rmtree(target_dir, ignore_errors=True)
         await clone_repo(repo, branch, target_dir)
+
+def check_repo_dir_config(repo_dir: Path, repo: str):
+    git_dir = repo_dir / '.git'
+    if git_dir.exists():
+        with open(git_dir / 'config') as f:
+            for line in f:
+                if line.strip().startswith('url ='):
+                    config_repo = line.split('=')[1].strip()
+                if config_repo == repo:
+                    return True
+    return False
 
 async def clone_repo(repo: str, branch: str, target_dir: Path):
     logger.info(f'⬇️ Cloning {repo}, branch: {branch} to {target_dir}...')
