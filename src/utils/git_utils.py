6f3ceb15394999c86facf2ff7ef811771e9a32b2
Chooook <egr311@ya.ru>
2025-09-12 16:25:15 +0300

    fix: remove git_token check, add if token exists - update command with bearer auth

--- src/utils/git_utils.py
+++ src/utils/git_utils.py
@@ -8,8 +8,8 @@ from loguru import logger
 
 async def ensure_repo(repo: str, branch: str, target_dir: Path):
     git_token = os.getenv('GIT_TOKEN', '')
-    if not git_token:
-        raise RuntimeError('GIT_TOKEN env var is not set')
+    # if not git_token:
+    #     raise RuntimeError('GIT_TOKEN env var is not set')
 
     if target_dir.exists() and check_repo_dir_config(target_dir, repo):
         await update_repo(repo, branch, git_token, target_dir)
@@ -24,19 +24,22 @@ def check_repo_dir_config(repo_dir: Path, repo: str):
             for line in f:
                 if line.strip().startswith('url ='):
                     config_repo = line.split('=')[1].strip()
-                if config_repo == repo:
-                    return True
+                    if config_repo == repo:
+                        return True
     return False
 
 async def clone_repo(repo: str, branch: str, git_token: str, target_dir: Path):
     logger.info(f'⬇️ Cloning {repo}, branch: {branch} to {target_dir}...')
     command = [
         'git', 'clone',
-        '-c', f'http.extraHeader=Authorization: Bearer {git_token}',
         '--depth', '1',
         '--branch', branch,
         repo, str(target_dir)
     ]
+    if git_token:
+        command.extend(
+            ['-c', f'http.extraHeader=Authorization: Bearer {git_token}']
+        )
     process = await asyncio.create_subprocess_exec(
         *command, stderr=asyncio.subprocess.PIPE)
     _, stderr = await process.communicate()
@@ -70,11 +73,11 @@ async def update_repo(repo: str, branch: str, git_token:str, target_dir: Path):
             error = stderr.decode()
             raise RuntimeError(f'{error}')
 
-    pull_command = [
-        'git',
-        '-c', f'http.extraHeader=Authorization: Bearer {git_token}',
-        'pull', 'origin', branch
-    ]
+    pull_command = ['git', 'pull', 'origin', branch]
+    if git_token:
+        pull_command.extend(
+            ['-c', f'http.extraHeader=Authorization: Bearer {git_token}']
+        )
     update_process = await asyncio.create_subprocess_exec(
         *pull_command, cwd=target_dir, stderr=asyncio.subprocess.PIPE)
     _, stderr = await update_process.communicate()
